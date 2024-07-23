4d58578085d7eb2b3343ef4fc65db42dca17b027
zeb33n <zeb.bond@33n.co.uk>
2024-07-23 16:53:20 +0100

    learning

--- list.c
+++ list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // use generic to get type?
 // give vector a type arg.
@@ -60,22 +61,30 @@ void vector_assign_value(Vector* vector, void* val, int index) {
   }
 }
 
+void* vector_get(Vector* vector, int index) {
+  void* out;
+  return memcpy(out, &vector->entries[index], get_effective_size(vector->type));
+}
+
 Vector* create_vector(Type type, int size) {
   Vector* ptr = (Vector*)malloc(sizeof(Vector));
 
   ptr->entries = malloc(get_effective_size(type) * size);
   ptr->maxvals = size;
-  ptr->numvals = size;
+  ptr->numvals = 0;
   ptr->type = type;
 
   return ptr;
 }
 
-int destroy_vector(Vector* vector) {
-  if (vector) {
-    free(vector->entries);
-    vector->maxvals = 0;
-    vector->numvals = 0;
+int destroy_vector(Vector** vector) {
+  Vector* v = *vector;
+  if (v) {
+    free(v->entries);
+    v->maxvals = 0;
+    v->numvals = 0;
+    v->type = 0;
+    *vector = NULL;
     return 0;
   } else {
     return -1;
@@ -92,6 +101,13 @@ int vector_resize(Vector* vector, int newsize) {
   } else {
     printf("Error: new size will cause data loss from vector\n");
     return -1;
+  }
+}
+
+void* vector_pop(Vector* vector, int index) {
+  void* out = vector_get(vector, index);
+  int i;
+  for (i = index; i > numvals; i++) {
   }
 }
 
@@ -146,12 +162,19 @@ int main(void) {
 
   x = create_vector(CHAR, 10);
   while (i) {
-    printf("do we make it\n");
     i--;
     vector_append(x, &i);
   }
 
   print_vector(x);
 
+  char* c = vector_get(x, 0);
+  char b = 1;
+  printf("%lu\n", sizeof(*c));
+  printf("%lu\n", sizeof(int));
+  vector_write(x, &b, 0);
+  printf("%i\n", *c);
+
+  destroy_vector(&x);
   return 0;
 }
